e7dae03bf8523f3687e7ad29f24c1d0a91d85dbd
riskiputraalamzah <riskialamzah1@gmail.com>
2026-07-07 19:27:52 +0700

    fix: update grid trust badges

--- src/components/landing/beranda-landing.tsx
+++ src/components/landing/beranda-landing.tsx
@@ -416,7 +416,7 @@ function HeroSection() {
             </a>
           </div>
 
-          <div className="mt-8 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4 2xl:mt-10">
+          <div className="mt-8 grid  gap-4 grid-cols-2 md:grid-cols-4 2xl:mt-10">
             {trustBadges.map((item) => (
               <div
                 key={item.title}
